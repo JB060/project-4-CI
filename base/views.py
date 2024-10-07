927b4d7a5b4c97eabe9154043224269442942f32
Jason Byrne <jasonbyrne060@gmail.com>
2024-10-07 09:36:36 +0000

    migrated all migarteions

--- base/views.py
+++ base/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 
+rooms = [
+    {'id' :1, 'name':'Lets learn python!'},
+    {'id' :2, 'name':'Design with me'},
+    {'id' :3, 'name':'Front end developers'},
+]
+
+
 def home(request):
     return render (request, 'home.html')
 
